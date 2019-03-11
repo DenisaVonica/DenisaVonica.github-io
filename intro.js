1e1bb045685d8e1a8d24387ea8340e6a4ce1f0b7
Denisa Vonica <vonicadenisa@yahoo.com>
2019-03-11 21:03:35 +0200

    cleanup js intro

--- intro.js
+++ intro.js
@@ -1,19 +1,3 @@
-console.info("my first js");
-
-var name = "Nicolae";
-var employed = true;
-
-console.warn("employed");
-console.warn(employed);
-
-console.info("name:");
-console.info(name);
-
-name='Matei'
-console.info("name:");
-console.info(name);
-
-
 function getWelcomeMsg(){
     return "Welcome to my site!";
 }
@@ -36,52 +20,6 @@ var summaryElement = document.getElementById("summary");
 console.info("summaryElement",summaryElement);
 var color = getColor();
 summaryElement.style.color = color ;
-
-//first example 
-
-function hideHomePage(){
-    var el = document.getElementById('home-page')
-    el.style.display = 'none';
-}
-function showSkillsPage(){
-    document.getElementById('skills-page').style.display ='block';
-}
-
-function clickOnSkills(){
-   // hideHomePage();
-    //showSkillsPage();
-    hidePage('home-page');
-    hidePage('languages-page');
-    hidePage('about-page');
-    showPage('skills-page');
-}
-
-function clickOnHome(){
-    // hideHomePage();
-     //showSkillsPage();
-     hidePage('skills-page');
-     hidePage('languages-page');
-     hidePage('about-page');
-     showPage('home-page');
- }
- function clickOnLanguages(){
-    // hideHomePage();
-     //showSkillsPage();
-     hidePage('home-page');
-     hidePage('skills-page');
-     hidePage('about-page');
-     showPage('languages-page');
- }
- function clickOnAbout(){
-    // hideHomePage();
-     //showSkillsPage();
-     hidePage('home-page');
-     hidePage('languages-page');
-     hidePage('skills-page');
-     showPage('about-page');
- }
-
-//first example-end
 
 
 function hidePage(page){
